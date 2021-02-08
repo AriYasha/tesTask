f2a94f4035c1f757c05f3a6d25b18a4a6ce66b64
Iryna <irinanootehnika@mail.ru>
2021-02-08 22:23:32 +0300

    SpringSecurity

---
+++ src/main/java/com/example/demo1/security/ApplicationUserRole.java
@@ -0,0 +1,19 @@
+package com.example.demo1.security;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+public enum ApplicationUserRole {
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet());
+
+
+    private final Set<ApplicationUserPermission> permissions;
+
+    ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+
+}
